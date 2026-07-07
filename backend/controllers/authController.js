b5782348b0fa8c17ba1b66b0c86d7d3ed41388b6
Okim-Tech <okimpeace187@gmail.com>
2026-07-07 14:39:42 +0100

    implement password hasging with bcrypt

---
+++ backend/controllers/authController.js
@@ -0,0 +1,48 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if all fields are filled
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "Please fill in all fields",
+      });
+    }
+
+    // Check if user already exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
