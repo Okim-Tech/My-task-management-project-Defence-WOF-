b2eeed5700e108eb9e2b53abe06135bf3f89a2a0
Okim-Tech <okimpeace187@gmail.com>
2026-07-07 12:22:16 +0100

    connect mongodb atlas database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,16 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const mongoose = require("mongoose");
-
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 15000,
+      socketTimeoutMS: 45000,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
     process.exit(1);
   }
 };
-
 module.exports = connectDB;
