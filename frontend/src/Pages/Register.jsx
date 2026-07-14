a8684e1adf99217d3559f3c8e15c57f27c6f47cc
Okim-Tech <okimpeace187@gmail.com>
2026-07-14 18:23:13 +0100

    complete frontend authentication

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,8 +1,103 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { User, Mail, Lock } from "lucide-react";
+
+import AuthLayout from "../layouts/AuthLayout";
+import Input from "../components/ui/Input";
+import Button from "../components/ui/Button";
+import { registerUser } from "../services/authService";
+
 function Register() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (formData.password !== formData.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    try {
+      await registerUser({
+        name: formData.name,
+        email: formData.email,
+        password: formData.password,
+      });
+
+      alert("Registration Successful!");
+
+      navigate("/");
+    } catch (error) {
+      alert(error.response?.data?.message || "Registration Failed");
+    }
+  };
+
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <AuthLayout title="Student Task Management" subtitle="Create your account">
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          name="name"
+          placeholder="Enter your name"
+          icon={<User size={18} />}
+          value={formData.name}
+          onChange={handleChange}
+        />
+
+        <Input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          icon={<Mail size={18} />}
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <Input
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          icon={<Lock size={18} />}
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm password"
+          icon={<Lock size={18} />}
+          value={formData.confirmPassword}
+          onChange={handleChange}
+        />
+
+        <Button text="Create Account" type="submit" />
+
+        <p
+          style={{
+            textAlign: "center",
+            marginTop: "20px",
+          }}
+        >
+          Already have an account? <Link to="/">Login</Link>
+        </p>
+      </form>
+    </AuthLayout>
   );
 }
 
