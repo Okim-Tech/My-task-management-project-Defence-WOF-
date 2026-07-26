5871a663e58654e3d0f0c465c0490190ac80925c
Okim-Tech <okimpeace187@gmail.com>
2026-07-26 11:34:27 +0100

    Fix taskService endpoint URL

--- frontend/src/Services/taskService.js
+++ frontend/src/Services/taskService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  "https://my-task-management-project-defence-wof.onrender.com/api/auth";
+  "https://my-task-management-project-defence-wof.onrender.com/api/tasks";
 
 // Authorization Header
 const getConfig = (token) => ({
